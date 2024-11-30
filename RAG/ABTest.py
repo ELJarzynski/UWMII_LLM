cc6a57b9ca6c00dc68fa53919e8155e17754284a
YoungHedgehog <92035751+ELJarzynski@users.noreply.github.com>
2024-11-30 22:50:47 +0100

    RAG using Ollama and OpenAPI, A/B tests, PDF loader

--- RAG/ABTest.py
+++ RAG/ABTest.py
@@ -2,7 +2,6 @@ import time
 from OpenAI import query_rag as openai_query
 from Ollama import query_rag as ollama_query
 
-# The query we want to test with both models
 query_text = "Number of hours per semester for databases"
 
 # Measure time and get response from Ollama
@@ -11,15 +10,15 @@ response_ollama = ollama_query(query_text)
 end_time_ollama = time.time()
 ollama_time = end_time_ollama - start_time_ollama
 
-# # Measure time and get response from OpenAI
-# start_time_openai = time.time()
-# response_openai = openai_query(query_text)
-# end_time_openai = time.time()
-# openai_time = end_time_openai - start_time_openai
+# Measure time and get response from OpenAI
+start_time_openai = time.time()
+response_openai = openai_query(query_text)
+end_time_openai = time.time()
+openai_time = end_time_openai - start_time_openai
 
 print(f"Ollama Response: {response_ollama}")
 print(f"Ollama Response Time: {ollama_time:.4f} seconds")
 
-# # Print the results
-# print(f"OpenAI Response: {response_openai}")
-# print(f"OpenAI Response Time: {openai_time:.4f} seconds\n")
+# Print the results
+print(f"OpenAI Response: {response_openai}")
+print(f"OpenAI Response Time: {openai_time:.4f} seconds\n")
